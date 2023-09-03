9783f2f0b963c5de54b5738f43d1d227647dd5a1
edney.silva <edney_contato@hotmail.com>
2023-09-03 17:02:10 -0300

    tests - created unit tests to domain layer to implement #23

---
+++ VaultApp/VaultDomain.Tests/Commands/RegisterUser/AsMaterializedUser_Should.cs
@@ -0,0 +1,15 @@
+﻿namespace VaultDomain.Tests.Commands.RegisterUser
+{
+    public class AsMaterializedUser_Should
+    {
+        [Fact]
+        public void Return_an_instance_from_User_with_same_values_on_the_command()
+        {
+            var command = new VaultDomain.Commands.RegisterUser.RegisterUserCommand("test@email.com", "validpassword");
+            var user = command.AsMaterializedUser();
+            Assert.NotNull(user);
+            Assert.Equal(user.UserName, command.UserName);
+            Assert.Equal(user.Password, command.Password);
+        }
+    }
+}
